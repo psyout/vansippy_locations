1314652249fdf50d511047980252756880e49a25
Felipe Gonzalez <contraviento@gmail.com>
2025-09-09 21:59:58 -0700

    configured spa routing server to load add location page

--- backend/server.js
+++ backend/server.js
@@ -3,30 +3,23 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import locationRoutes from './routes/location.route.js';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
+
+// Define the port using an environment variable or default to 8080
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
+
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-// API routes
+// Route to connect endpoints
 app.use('/api/locations', locationRoutes);
 
-// Static files
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-// Catch-all route for React Router (must be last)
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
+// Middleware to Connect to MongoDB
 app.listen(PORT, () => {
 	connectDB();
 	console.log(`Server started at ${PORT}`);
