56141a1c2969ba4cececede2872cd587c7a45f73
Felipe Gonzalez <contraviento@gmail.com>
2025-09-09 22:09:11 -0700

    Add .htaccess file to enable SPA routing for React Router on Hostinger

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import locationRoutes from './routes/location.route.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
@@ -15,6 +16,11 @@ app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+// After all API routes and static middleware
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // Route to connect endpoints
 app.use('/api/locations', locationRoutes);
